9357437decca7d8a93818beb5d0992ea34955e8e
Dorian Bouchet <dorian.bouchet@univ-grenoble-alpes.fr>
2025-06-17 10:22:47 +0200

    remove transition

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
-  // Init Masonry
-  var $grid = $(".grid").masonry({
-    gutter: 10,
-    horizontalOrder: true,
-    itemSelector: ".grid-item",
-    transitionDuration: 0,
-  });
-  // Layout Masonry after each image loads
-  $grid.imagesLoaded().progress(function () {
-    $grid.masonry("layout");
+  var $grid = $(".grid");
+
+  // Wait until all images are fully loaded
+  $grid.imagesLoaded(function () {
+    $grid.masonry({
+      gutter: 10,
+      horizontalOrder: true,
+      itemSelector: ".grid-item",
+    });
   });
 });
